b1ba408fe8d6ecb366e01b0a32f8325a35783f9e
Vadim Tropashko <vadim.tropashko@oracle.com>
2023-10-30 14:17:05 -0700

    Finished error diagnostics

--- src/errorMsgs.js
+++ src/errorMsgs.js
@@ -1,7 +1,8 @@
+import split_str from './split_str.js';
 
 const findErrors = (function () {
 
-    function ErrorMsg( message, from, to, severity ) {
+    function SyntaxError( message, from, to, severity ) {
         this.from = from;
         this.to = to;
         if( to == null )
@@ -17,30 +18,33 @@ const findErrors = (function () {
         this.depth = depth;   // 0-based
     }
     
-    function checkSyntax( ddlInstance ) {
+    function checkSyntax( ddlInstance, input ) {
         const ddl = ddlInstance;
 
         let ret = [];
 
+        const lines = input.split("\n");
+    
+        ret = ret.concat(line_mismatch(lines));
         const descendants = ddl.descendants();
  
         for( let i = 0; i < descendants.length; i++ ) {
             const node = descendants[i];
             if( ddl.optionEQvalue('genpk', true) && descendants[i].parseName() == 'id' ) {
                 const depth = node.content.toLowerCase().indexOf('id');
-                ret.push(new ErrorMsg( messages.duplicateId, new Offset(node.line, depth) ));
+                ret.push(new SyntaxError( messages.duplicateId, new Offset(node.line, depth) ));
                 continue;
             }
             const src2 = node.src[2];
             if( 2 < node.src.length && src2.value == '-' ) {
                 const depth = src2.begin;
-                ret.push(new ErrorMsg(  messages.invalidDatatype, new Offset(node.line,depth) ));
+                ret.push(new SyntaxError(  messages.invalidDatatype, new Offset(node.line,depth) ));
                 continue;
             }
             const src1 = node.src[1];
             if( 1 < node.src.length && 0 < src1.value.indexOf('0') ) {
                 const depth = src1.begin;
-                ret.push(new ErrorMsg( messages.invalidDatatype, new Offset(node.line,depth) ));
+                ret.push(new SyntaxError( messages.invalidDatatype, new Offset(node.line,depth) ));
                 continue;
             }
 
@@ -54,20 +58,23 @@ const findErrors = (function () {
     function ref_error_in_view( ddl, node ) {
         var ret  = [];
         
-        var nodeUpperContent = node.trimmedContent().toUpperCase();
+        var line = node.content.toLowerCase();
         if( node.parseType() == 'view' ) {
-            var chunks = nodeUpperContent.split(' ');
-            for( var j = 2; j < chunks.length; j++ ) { 
-                if( chunks[j].trim() == "" )
+            var chunks = split_str(line,' '); //line.split(' ');
+            let pos = 0;
+            for( var j = 0; j < chunks.length; j++ ) { 
+                pos += chunks[j].length;
+                if( chunks[j] == ' ' ) 
                     continue;
-                if( 0 == chunks[j].indexOf("/") )
+                if( chunks[j] == 'view' ) 
                     continue;
-                var tbl = ddl.find(chunks[j]);
+                if( j == 1 ) 
+                    continue;
+               var tbl = ddl.find(chunks[j]);
                 if( tbl == null ) {
-                    var pos = nodeUpperContent.indexOf(chunks[j]);
-                    ret.push( new ErrorMsg(
+                    ret.push( new SyntaxError(
                         messages.undefinedObject+chunks[j],
-                        new Offset(node.line, pos)
+                        new Offset(node.line, pos-chunks[j].length)
                     ));
                 }
             }
@@ -79,10 +86,12 @@ const findErrors = (function () {
         var ret  = [];
         var line = node.content.toLowerCase();
         if( 0 < line.indexOf("/fk") || 0 < line.indexOf("/reference") ) {
-            var chunks = line.split(' ');
-            var refIsNext = false;
-            for( var j = 1; j < chunks.length; j++ ) { 
-                if( chunks[j].trim() == "" )
+            let chunks = split_str(line,' '); //line.split(' ');
+            let pos = 0;
+            let refIsNext = false;
+            for( var j = 0; j < chunks.length; j++ ) { 
+                pos += chunks[j].length;
+                if( chunks[j] == ' ' )
                     continue;
                 if( chunks[j] == "/fk" || 0 == chunks[j].indexOf("/reference") ) {
                     refIsNext = true;
@@ -92,10 +101,9 @@ const findErrors = (function () {
                     continue;
                 var tbl = ddl.find(chunks[j]);
                 if(  tbl == null ) {
-                    var pos = line.indexOf(chunks[j]);
-                    ret.push( new ErrorMsg(
+                    ret.push( new SyntaxError(
                         messages.undefinedObject+chunks[j],
-                        new Offset(node.line, pos)
+                        new Offset(node.line, pos-chunks[j].length)
                     ));                   
                     break;
                 }
@@ -104,110 +112,72 @@ const findErrors = (function () {
     return ret;
     }
     
-
+    function line_mismatch( lines ) {
+        var ret  = [];
+        
+        var indent = guessIndent( lines )
+        
+        for( var i = 1; i < lines.length; i++ ) {
+            var priorline = lines[i-1];
+            var line = lines[i];
+            
+            var priorIndent = depth(priorline);
+            var lineIndent = depth(line);
+            
+            if( lineIndent == 0 )
+                continue;
+           
+            if( priorIndent < lineIndent && lineIndent < priorIndent+indent )
+                ret.push(new SyntaxError(
+                    messages.misalignedAttribute+indent,
+                    new Offset(i, lineIndent)
+                )
+            );
+        }
+    
+        return ret;
+    }
     return checkSyntax;
 }());
 
-function parse_errors( input ) {
-    var ret  = [];
-    
-    var lines = input.split("\n");
-    
-    if( lines.length < 5 || ddl == null )
-        return ret;
-    
-    ret = ret.concat(line_mismatch(lines));
-    ddl.translate(input);
-    ret = ret.concat(ref_error(lines));
 
-    return ret;
-}
-
-function line_mismatch( lines ) {
-    var ret  = [];
-    
-    var indent = guessIndent( lines )
-    
-    for( var i = 1; i < lines.length; i++ ) {
-        var priorline = lines[i-1];
-        var line = lines[i];
-        
-        var priorIndent = apparentDepth(priorline);
-        var lineIndent = apparentDepth(line);
-        
-        if( lineIndent == 0 )
-            continue;
-       
-        if( priorIndent < lineIndent && lineIndent < priorIndent+indent )
-            ret.push({
-                        from: {line:i, ch:lineIndent,  },
-                        to: {line:i, ch:lineIndent+1,  },
-                        //severity: "error",
-                        message: "Misaligned code. \nThe indent appears to be "+indent+" spaces."
-            });
-    }
-
-    return ret;
-}
 
 function guessIndent( lines ) {    	
-    var idents = [];
-    
-    var priorFullIndent = -1;
-    var priorRelativeIndents = [];
-    
+    let depths = [];
+
     for( var i = 0; i < lines.length; i++ ) {
         var line = lines[i];
-        if( "\n" == line )
-            continue;
-        
-        var ident = apparentDepth(line);
-        
-        if( priorFullIndent == -1 ) {
-            priorFullIndent = ident;
-            priorRelativeIndents.push(ident);
-            continue;
-        }
-        var index = ident - priorFullIndent;
-        if( index == 0 ) {
-            var tmp = priorRelativeIndents[priorRelativeIndents.length-1];
-            if( tmp != 0 )
-                index = tmp;
-        }
-        if( index < 0 ) {
-            index = -index;
-            priorRelativeIndents.length--;
-        } else {
-            if( priorFullIndent < ident)
-                priorRelativeIndents.push(index);
-        }
-        if( index != 0 ) {
-            if( idents[index] == null )
-                idents[index] = 0;
-            idents[index]++;
-        }           
-        priorFullIndent = ident;
+        depths[i] = depth(line);
     }
-    var ret = 1;
-    var cmp = idents[ret];
-    if( cmp == null )
-        cmp = 0;
-    for( var i = 1; i < idents.length; i++ ) {
-        if( cmp < idents[i] ) {
-            ret = i;
-            cmp = idents[i];
+
+    let frequencies = [];
+    for( let i = 0; i < depths.length; i++ ) {
+        let j = parentIndex(depths, i);
+        if( j != null ) {
+            let f = frequencies[depths[i]-depths[j]];
+            if( f == null )
+                f = 0;
+            frequencies[depths[i]-depths[j]] = f+1;
         }
     }
-    return ret;
+
+    let indent = null;
+    for( let i in frequencies ) {
+        if( indent == null || frequencies[indent] <= frequencies[i] )
+            indent = i; 
+    }
+    return indent;
+
 }
 
-function apparentDepth( line ) {
+function depth( line ) {
     var chunks = line.split(/ |\t/);
     var offset = 0;
     for( var j = 0; j < chunks.length; j++ ) {
-        var chunk = chunks[j]/*.intern()*/;
-        //if( "\t" == chunk )
-            //TODO;
+        var chunk = chunks[j];
+        if( "\t" == chunk ) {
+            offset += 4; //TODO;
+        }
         if( "" == chunk  ) {
             offset++;
             continue;
@@ -218,12 +188,19 @@ function apparentDepth( line ) {
     return 0;
 }
 
+function parentIndex( depths, lineNo ) {
+    for( let i = lineNo; 0 <= i; i-- ) 
+        if( depths[i] < depths[lineNo] )
+            return i;
+    return null;
+}
 
 
 const messages = {
     duplicateId: 'Explicit ID column conflicts with genpk',
     invalidDatatype: 'Invalid Datatype',
     undefinedObject: 'Undefined Object: ',
+    misalignedAttribute: 'Misaligned Table or Column; apparent indent = ',
 }
 
 export default {findErrors, messages};
